a12e3a942f03fc05090e6cfe693465f12bfc4624
designjorgerodriguez-svg <designjorgerodriguez@gmail.com>
2026-08-10 14:40:42 +0200

    Add service worker for installable mobile app

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='viaje-francia-v1';
+const APP=['./','./index.html','./places.js','./beaches.js','./inland.js','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP)));self.skipWaiting()});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;const url=new URL(e.request.url);if(url.origin!==location.origin)return;e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>cached)))});
