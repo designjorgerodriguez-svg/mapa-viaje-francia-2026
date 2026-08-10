eea199c677250c3bfdadd4b374fdfbced356778e
designjorgerodriguez-svg <designjorgerodriguez@gmail.com>
2026-08-10 21:52:16 +0200

    Bump app cache for cobalt redesign

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='viaje-francia-v11';
+const CACHE='viaje-francia-v12';
 const APP=['./','./index.html','./theme.css','./places.js','./beaches.js','./inland.js','./ui-v4.js','./ui-v4-fix.js','./ui-v8-popup.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
@@ -7,7 +7,7 @@ self.addEventListener('fetch',e=>{
   const url=new URL(e.request.url);
   if(url.origin!==location.origin)return;
   e.respondWith(
-    fetch(e.request).then(res=>{
+    fetch(e.request,{cache:'no-store'}).then(res=>{
       if(res&&res.ok){const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy))}
       return res;
     }).catch(()=>caches.match(e.request))
