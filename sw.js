1c9e35c4bae58fbb690de6ac24b60ee53143c568
designjorgerodriguez-svg <designjorgerodriguez@gmail.com>
2026-08-10 18:00:43 +0200

    Refresh PWA cache after UI redesign

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='viaje-francia-v1';
+const CACHE='viaje-francia-v2';
 const APP=['./','./index.html','./places.js','./beaches.js','./inland.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
