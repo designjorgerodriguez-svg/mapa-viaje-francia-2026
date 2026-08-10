276924d466e102a6978ccb28ea454f1f8ce79faf
designjorgerodriguez-svg <designjorgerodriguez@gmail.com>
2026-08-10 21:39:07 +0200

    Refresh blue UI cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='viaje-francia-v10';
+const CACHE='viaje-francia-v11';
 const APP=['./','./index.html','./theme.css','./places.js','./beaches.js','./inland.js','./ui-v4.js','./ui-v4-fix.js','./ui-v8-popup.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
