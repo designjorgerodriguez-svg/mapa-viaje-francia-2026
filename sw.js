a53ca89c9aae7d70b88d1137d9a333cafed8f5af
designjorgerodriguez-svg <designjorgerodriguez@gmail.com>
2026-08-10 22:16:20 +0200

    Bump PWA cache for embedded icons

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='viaje-francia-v14';
+const CACHE='viaje-francia-v15';
 const APP=['./','./index.html','./theme.css','./places.js','./beaches.js','./inland.js','./ui-v4.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
